745bd90e79c16900459a69b63952f7357337f18a
nguyenvansonk35a3 <nguyenvansonk35a3@gmail.com>
2019-09-13 21:06:53 +0700

    changer repository

--- src/main/java/vn/ladudu/demospringboot/repository/IStudentRepository.java
+++ src/main/java/vn/ladudu/demospringboot/repository/IStudentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import vn.ladudu.demospringboot.model.Student;
 
 public interface IStudentRepository extends JpaRepository<Student, Long> {
+//    nothiung
 }
